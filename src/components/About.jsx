052b850227483e53cfc6cc68a5810709909cf65e
AmolShelke2 <shelkeamol897@gmail.com>
2024-02-14 19:15:35 +0530

    minor changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,9 @@ export const About = () => {
       <div className="flex justify-center items-center gap-12 mt-12">
         {aboutData.map((data) => {
           return (
-            <div className="h-[200px]  py-2 text-white" key={data.number}>
+            <div
+              className="h-auto w-[300px] shadow-lg bg-[#101419] hover:shadow-2xl cursor-pointer py-4 text-white px-4 rounded-lg"
+              key={data.number}>
               <h3 className="text-[50px] font-bold my-2">{data.number}</h3>
               <p className="text-lg my-2">{data.count}</p>
               <span className="text-sm text-gray-300">{data.description}</span>
