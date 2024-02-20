c6a188e635eaf7a743d28abf1086859ad27c9f13
AmolShelke2 <shelkeamol897@gmail.com>
2024-02-20 18:52:14 +0530

    finalized manual trade page

--- src/components/manual-trade/ManualPositionsTabs.jsx
+++ src/components/manual-trade/ManualPositionsTabs.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { AiOutlineDownload, AiOutlineSearch } from "react-icons/ai";
+import { DynamicTable } from "./DynamicTable";
 
 export const ManualPositionsTabs = () => {
   const [currentTab, setCurrentTab] = useState("Open Positions");
@@ -35,6 +37,34 @@ export const ManualPositionsTabs = () => {
           <p className=" capitalize text-blue-500 text-sm">Order History</p>
         </div>
       </div>
+
+      {/* delete download and search header */}
+      <div className="flex h-[45px] justify-between w-full gap-2 px-4 border-b my-1 py-2">
+        <div className="flex items-center gap-5">
+          <button className="bg-[#ea5455] text-white px-6 py-1 border-none outline-none rounded-lg">
+            Delete
+          </button>
+          <button className="bg-[#f6f6f6] px-6 py-1 border-none outline-none rounded-lg">
+            <AiOutlineDownload size={24} className="text-gray-500" />
+          </button>
+        </div>
+        <div>
+          <div className="flex items-center">
+            <div className="py-1 px-2 border cursor-pointer">
+              <AiOutlineSearch size={24} className="text-gray-500" />
+            </div>
+
+            <input
+              type="text"
+              placeholder="search"
+              className="w-[350px] border focus:outline-none bg-transparent py-1 px-4 rounded-sm"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* dynamic table depend on tabs */}
+      <DynamicTable tab={currentTab} />
     </div>
   );
 };
